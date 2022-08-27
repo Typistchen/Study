9cd1ab952fecac1b1dd8640241a6cd93b512fe8f
Typist_chen <1274006015@qq.com>
2022-08-27 21:37:22 +0800

    Third day commit2

--- Day4/Opencv4.py
+++ Day4/Opencv4.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 img = cv2.imread('D:\\WorkRoom\\postgraduate\\Study\\Day2\\1.png')
 top_size, bottom_size, left_size, right_size = (50, 50, 50, 50)
-
+cv2.imshow('image', img)
+cv2.waitKey(0)
 replicate = cv2.copyMakeBorder(img, top_size, bottom_size, right_size, left_size, borderType=cv2.BORDER_REPLICATE)
 reflect = cv2.copyMakeBorder(img, top_size, bottom_size, right_size, left_size, borderType=cv2.BORDER_REFLECT)
 reflect01 = cv2.copyMakeBorder(img, top_size, bottom_size, right_size, left_size, borderType=cv2.BORDER_REFLECT_101)
@@ -17,3 +18,36 @@ plt.subplot(234), plt.imshow(reflect01, 'gray'), plt.title('REFELECT01')
 plt.subplot(235), plt.imshow(wrap, 'gray'), plt.title('WRAP')
 plt.subplot(236), plt.imshow(constant, 'gray'), plt.title('CONSTANT')
 plt.show()
+
+
+# 阈值
+img2 = cv2.imread('D:\\WorkRoom\\postgraduate\\Study\\Day2\\1.png', cv2.IMREAD_GRAYSCALE )
+ret, thresh1 = cv2.threshold(img2, 127, 255, cv2.THRESH_BINARY)
+ret, thresh2 = cv2.threshold(img2, 127, 255, cv2.THRESH_BINARY_INV)
+ret, thresh3 = cv2.threshold(img2, 127, 255, cv2.THRESH_TRUNC)
+ret, thresh4 = cv2.threshold(img2, 127, 255, cv2.THRESH_TOZERO)
+ret, thresh5 = cv2.threshold(img2, 127, 255, cv2.THRESH_TOZERO_INV)
+
+titles = ['Oroginal Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img2, thresh1, thresh2, thresh3, thresh4, thresh5]
+for i in range(6):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+
+plt.show()
+# 均值滤波
+blur = cv2.blur(img, (3,3))
+
+cv2.imshow('blur', blur)
+cv2.waitKey(0)
+
+# 高斯滤波
+aussion = cv2.GaussianBlur(img, (5, 5), 1)
+cv2.imshow('sussion', aussion)
+cv2.waitKey(0)
+
+# 腐蚀操作
+
+
+
