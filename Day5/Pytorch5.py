d434eff86489dd62b260fb8432c54e15b26fa91c
Typist_chen <1274006015@qq.com>
2022-08-28 16:25:24 +0800

    Forth day commit2

---
+++ Day5/Pytorch5.py
@@ -0,0 +1,15 @@
+# 卷积神经网络
+import torch
+import torch.nn as nn
+import os
+# layer = nn.Conv2d(1, 3, kernel_size=3, stride=1, padding=0)
+# x = torch.rand(1, 1, 28, 28)
+#
+# out = layer.forward(x)
+# print('out:', out.size())
+#
+# x = torch.rand(100, 16, 784)
+# layer = nn.BatchNorm1d(16)
+# out = layer(x)
+# print('layer:', layer.running_mean)
+print(os.path.join('root', 'name'))
